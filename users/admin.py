9e6cf54b414cf5b80cdd1e9363dbc678b61bee8a
Antis159 <56255270+Antis159@users.noreply.github.com>
2019-10-16 13:50:49 +0300

    Updated admin functions

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
 
+admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    list_display_links = ['username']
+
+admin.site.register(User, UserAdmin)
 # Register your models here.
