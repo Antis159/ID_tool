671fe0b8ee00c4863489bab861e267bad8e6e839
Antis159 <56255270+Antis159@users.noreply.github.com>
2019-10-16 13:44:33 +0300

    updated static dir in production

--- id_tool/settings.py
+++ id_tool/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')]
